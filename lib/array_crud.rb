a16dbecbed4658978c42530005e56fc02ea01701
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-06-23 15:27:04 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,33 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
+  ["a","b","c","d"]
+end
+
+def add_element_to_end_of_array(array, element)["a", "b", "c", "d"] << "arrays!"
   
 end
 
-def add_element_to_end_of_array(array, element)
+def add_element_to_start_of_array(array, element)["a", "b", "c", "d"].unshift("wow")
+end
+
+def remove_element_from_end_of_array(array)["a", "b", "c", "d", "arrays!"].pop
   
 end
 
-def add_element_to_start_of_array(array, element)
+def remove_element_from_start_of_array(array)["wow", "b", "c", "d"].shift
   
 end
 
-def remove_element_from_end_of_array(array)
+def retrieve_element_from_index(array, index_number)"am"
   
 end
 
-def remove_element_from_start_of_array(array)
-  
+def retrieve_first_element_from_array(array)["wow","b","c"].first
 end
 
-def retrieve_element_from_index(array, index_number)
-  
-end
-
-def retrieve_first_element_from_array(array)
-  
-end
-
-def retrieve_last_element_from_array(array)
+def retrieve_last_element_from_array(array) [ "a", "arrays!"].last
   
 end
