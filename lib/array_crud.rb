146ec3210b37cf9a4c69708e685c55884a1ee001
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-06-23 15:22:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,7 +26,7 @@ def retrieve_element_from_index(array, index_number)["wow", "b", "c", "d"]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  []
 end
 
 def retrieve_last_element_from_array(array)
